31f8e57afb57e241a71f74d75d5212a35417ebfa
csw823197541 <823917541@qq.com>
2018-05-14 10:44:30 +0800

    测试提交

--- src/routes/Info/MaterialInfo.js
+++ src/routes/Info/MaterialInfo.js
@@ -2,7 +2,21 @@
  * Created by chennanjin on 2018/5/9.
  */
 import React, { Component } from 'react';
+import { Button, Input, Table, Pagination, Modal, Popover, Radio, Form, Select, Spin} from 'antd';
+import { connect } from 'dva';
+import _ from 'lodash';
+import styles from './MaterialInfo.less'
 
+const InputSearch = Input.Search;
+const RadioGroup = Radio.Group;
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+@connect(({userInfoManager}) => ({
+    userInfoManager,
+  }),
+)
+@Form.create()
 export default class Application extends Component {
 
   render() {
