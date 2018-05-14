e526b76e0bfc29c0ca87005345a007d4de6566b9
csw823197541 <823917541@qq.com>
2018-05-14 10:22:55 +0800

    1

--- src/routes/Info/MaterialInfo.js
+++ src/routes/Info/MaterialInfo.js
@@ -6,6 +6,6 @@ import React, { Component } from 'react';
 export default class Application extends Component {
 
   render() {
-    return <div> 基础信息管理-物料信息</div>
+    return <div> 基础信息管理-物料信息wu</div>
   }
 }
